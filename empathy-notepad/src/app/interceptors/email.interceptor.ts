714af4d495a8c6beb4de6a2238769d9485e7b24c
Simon Mac Aree <smacr25@gmail.com>
2025-12-09 13:20:15 +0000

    Adding interceptor
    Adding interceptor for email with api and updating the app.config.ts file.

---
+++ empathy-notepad/src/app/interceptors/email.interceptor.ts
@@ -0,0 +1,12 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const emailInterceptor: HttpInterceptorFn = (req, next) => {
+      const myEmailAddress = 'smacr25@gmail.com';
+      const emailHeaderKey = 'smacr25-email';
+
+      const authReg = req.clone({
+          headers: req.headers.set(emailHeaderKey, myEmailAddress)
+      });
+
+      return next(authReg);
+}
